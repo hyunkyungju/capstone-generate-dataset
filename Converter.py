d8471ddfe2a3e3bb221e8f49ec97bae7f473bbf2
hyunkyungju <wngusrudrid@naver.com>
2022-04-06 10:48:17 +0900

    Feat: tqdm 도입
    진행 상황을 보기 위해서 tqdm을 도입합니다.

--- Converter.py
+++ Converter.py
@@ -1,10 +1,12 @@
 from pdf2image import convert_from_path
 import os
+import tqdm
 
 
 def convert_pdf_to_img(input_path, output_path):
     print("start convert from pdf to img")
-    for input_file in os.listdir(input_path):
+
+    for input_file in tqdm(os.listdir(input_path), desc="convert pdf -> image"):
         os.mkdir(output_path + input_file[:-4] + "/")
         pages = convert_from_path(input_path + input_file)
         for i, page in enumerate(pages):
