9ee208c964261b38604a83a36c96a8a58f21961b
hyunkyungju <wngusrudrid@naver.com>
2022-04-10 09:07:03 +0900

    Feat: NeurIPS invitation 정보 추가
    뉴립스 크롤링을 시도합니다.

--- Crawler.py
+++ Crawler.py
@@ -6,45 +6,79 @@ from tqdm import tqdm
 import openreview
 
 
-def download_iclr21(client, outdir='./'):
-    print('getting metadata...')
-    # get all ICLR '19 submissions, reviews, and meta reviews, and organize them by forum ID
-    # (a unique identifier for each paper; as in "discussion forum").
+# [error] forum: _qJXkf347k
+
+def crawl_iclr(pdf_path, year):
+    client = openreview.Client(
+        baseurl='https://api.openreview.net',
+        username='',
+        password=''
+    )
+
+    print(f'start crawl iclr {year}')
+
+    # Get submission lists
     submissions = openreview.tools.iterget_notes(
-        client, invitation='ICLR.cc/2021/Conference/-/Blind_Submission')
+        client, invitation=f'NeurIPS.cc/{year}/Conference/-/Blind_Submission') # NeurIPS.cc/2021/Conference/-/Blind_Submission
     submissions_by_forum = {n.forum: n for n in submissions}
     print(f"number of forums: {submissions_by_forum.__len__()}")
 
-    # Build a list of metadata.
-    # For every paper (forum), get the review ratings, the decision, and the paper's content.
+    # Get reviews
+    '''
+    2021: 2595, 2979    
+    2020: 2213, 2561
+    2019: 1419, 1565
+    
+    뉴립스
+    2021: 2769, 2763
+    '''
+
+    #if year == "2019":
+    reviews1 = openreview.tools.iterget_notes(
+        client, invitation=f'NeurIPS.cc/{year}/Conference/-/Paper.*/Official_Comment')
+    print(f"number of reviews1: {reviews1.__sizeof__()}")
+    #else:
+    reviews2 = openreview.tools.iterget_notes(
+        client, invitation=f'NeurIPS.cc/{year}/Conference/Paper.*/-/Official_Comment')
+    print(f"number of reviews2: {reviews2.__sizeof__()}")
+    '''
+    reviews_by_forum = defaultdict(list)
+    for review in reviews:
+        reviews_by_forum[review.forum].append(review)
+    print(f"number of reviews: {reviews_by_forum.__len__()}")
+
+    # Build a list of metadata
     metadata = []
     for forum in submissions_by_forum:
+        forum_reviews = reviews_by_forum[forum]
+        review_ratings = [n.content['rating'] for n in forum_reviews]
+        value = 0
+        for review in review_ratings:
+            value += int(review[0])
+        rating = value / len(review_ratings)
         forum_metadata = {
             'forum': forum,
+            'rating': rating
         }
         metadata.append(forum_metadata)
 
-    print('start to save pdf')
+    print('writing metadata to file')
+    with open(os.path.join("./", f'iclr{year}_metadata.jsonl'), 'w') as file_handle:
+        for forum_metadata in metadata:
+            file_handle.write(json.dumps(forum_metadata) + '\n')
 
-    # os.makedirs(outdir)
-    for forum_metadata in tqdm(metadata, desc='getting pdfs'):
+    outdir = pdf_path+"iclr"+year+"/"
+    print('start to save pdf')
+    os.makedirs(outdir)
+    for forum_metadata in tqdm(metadata, desc=f'iclr {year} getting pdfs'):
         try:
             pdf_binary = client.get_pdf(forum_metadata['forum'])
             pdf_outfile = os.path.join(outdir, '{}.pdf'.format(forum_metadata['forum']))
             with open(pdf_outfile, 'wb') as file_handle:
                 file_handle.write(pdf_binary)
         except:
-            print("[error] forum:", forum_metadata['forum'])
-# [error] forum: _qJXkf347k
-
-def crawl_iclr_2021(pdf_path):
-    client = openreview.Client(
-        baseurl='https://api.openreview.net',
-        username='',
-        password=''
-    )
-    download_iclr21(client, pdf_path)
-
+            print(f"[error][crawl pdf][iclr {year}] forum: {forum_metadata['forum']}")
+'''
 
 class Crawler:
     def __init__(self):
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import torch
 from torch.utils.data import DataLoader
 
 from torchvision import transforms
-
+import openreview
 from Converter import convert_pdf_to_img
-from Crawler import crawl_iclr_2021
+from Crawler import crawl_iclr
 from paper_data_set import PaperDataSet
 
 
@@ -39,14 +39,52 @@ def make_save_data_set(image_path, dataset_file_name):
 
 
 if __name__ == '__main__':
-    pdf_path = "../dataset/pdf/iclr21/"
-    image_path = "../dataset/image/iclr21/"
+    pdf_path = "../dataset/pdf/"
+    image_path = "../dataset/image/"
     dataset_file_name = 'dataset.pt'
 
-    # crawl_iclr_2021(pdf_path)
+    # crawl_iclr(pdf_path, "2021")
 
-    convert_pdf_to_img(pdf_path, image_path)
+    client = openreview.Client(
+        baseurl='https://api.openreview.net',
+        username='',
+        password=''
+    )
 
-    make_save_data_set(image_path, dataset_file_name)
+    year = "2021"
+
+    iclr19_invitations_iterator = openreview.tools.iterget_invitations(
+        client, regex='NeurIPS.cc/2020/Conference')
+
+    for invitation in iclr19_invitations_iterator:
+        print(invitation.id)
+# Public_Comment
+
+    """
+    NeurIPS.cc/2021/Conference/-/Reduced_Load
+    NeurIPS.cc/2021/Conference/-/Recruit_Reviewers
+    NeurIPS.cc/2021/Conference/-/Recruit_Ethics_Reviewers
+    NeurIPS.cc/2021/Conference/-/Blind_Submission
+    NeurIPS.cc/2021/Conference/Paper79/-/Withdraw
+    NeurIPS.cc/2021/Conference/Paper11535/-/Official_Comment
+    NeurIPS.cc/2021/Conference/Paper5854/-/Decision
+    NeurIPS.cc/2021/Conference/Paper327/-/Public_Comment
+    NeurIPS.cc/2021/Conference/Paper10608/-/Desk_Reject
+    """
+
+    #reviews2 = openreview.tools.iterget_notes(
+    #   client, invitation='NeurIPS.cc/2021/Conference/Paper.*/-/Withdraw')
+    #  =f'NeurIPS.cc/{year}/Conference/Paper.*/-/Official_Review')
+    #print(sum(1 for _ in reviews2))
+
+    #for r in reviews2:
+    #    print(r)
+    #    break
+
+    # 리뷰가 10749개나 된다... 뭐지.. 
+
+    # convert_pdf_to_img(pdf_path, image_path)
+
+    # make_save_data_set(image_path, dataset_file_name)
 
     # data_set_usage_ex(dataset_file_name)
