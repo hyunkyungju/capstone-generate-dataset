8de588ed9ddce144e5a2229c6f5966f0c0c591fd
hyunkyungju <wngusrudrid@naver.com>
2022-06-01 17:05:12 +0900

    Chore: img to bin 폴더 정리

--- convert-img-to-bin/ImageToBinaryConverter.py
+++ convert-img-to-bin/ImageToBinaryConverter.py
@@ -3,17 +3,16 @@ import os
 import numpy as np
 from tqdm import tqdm
 
-def image_to_binary_cut(input_path, output_path):
+def image_to_binary(input_path, output_path):
     input_folders = os.listdir(input_path)
     for input_folder in tqdm(input_folders, desc="convert jpg -> bin"):
         output_folder = output_path + input_folder
         os.mkdir(output_folder)
-        for i in range(1, 9):
+        for i in range(9):
             path = input_path+input_folder+"/"+str(i+1)+".jpg"
             img_cv = cv2.imread(path)
             if img_cv is None:
-                img_cv = cv2.imread("white.jpg")
-            img_cv = img_cv[110:, :]
+                img_cv = cv2.imread("../white.jpg")
             resized_cv = cv2.resize(img_cv, dsize=(224, 224), interpolation=cv2.INTER_AREA)
             binary_cv = cv2.imencode('.PNG', resized_cv)[1].tobytes()
             binary_file = f'{output_folder}/{i + 1}.bin'
@@ -21,8 +20,10 @@ def image_to_binary_cut(input_path, output_path):
                 f.write(binary_cv)
 
 
-def binary_to_image_cut():
-    binary_file = "../dataset/binary/iclr2021_cut/_77KiX2VIEg/2.bin"
+
+
+def binary_to_image():
+    binary_file = "../test.bin"
     with open(binary_file, 'rb') as f:
         data = f.read()
     encoded_img = np.fromstring(data, dtype=np.uint8)
